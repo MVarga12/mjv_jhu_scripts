e86c4475b42e52f30c8df82adca0636299658985
Matthew Varga <mvarga@JanusMac.local>
2017-10-17 09:34:17 -0400

    changed to allow user to alter column before visualizing

--- nfsim_analysis_scripts/gdat_plot.py
+++ nfsim_analysis_scripts/gdat_plot.py
@@ -8,6 +8,13 @@ target = sys.argv[2]    # target specie
 d = [line.split() for line in open(filename,"r")]
 del d[0][0] # delete that stupid pound sign
 
+ans = raw_input("Alter column (y/n)?")
+if ans == 'y':
+    cons = raw_input("Divide column by what symmetry? (integer) ")
+    cons = int(cons)
+else:
+    cons = 1
+
 val = 0
 for i in range(0,len(d[0])):
     if d[0][i] == target:
@@ -21,7 +28,7 @@ for i in range(1,len(d)):
     targlist.append(d[i][val])
 
 for i in range(0,len(time)):
-    out.append([float(time[i]),float(targlist[i])])
+    out.append([float(time[i]),float(targlist[i])/cons])
 
 f = open('tmp','w')
 for i in range(0,len(out)):
