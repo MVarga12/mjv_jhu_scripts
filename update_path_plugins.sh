f01ff45d85d190f806e72753532d8ca7ca1b4504
Matthew Varga <matthewjvarga@gmail.com>
2018-03-29 15:03:32 -0400

    forgot to copy the updated update_path_plugins.sh script

--- update_path_plugins.sh
+++ update_path_plugins.sh
@@ -1,13 +1,21 @@
 #!/usr/local/bin/fish
+# short script to update all vim plugins (tested for pathogen)
+# written for fish shell
 
 set cwd (pwd)
 cd ~/.vim/bundle/
 
+if test -e ~/Google\ Drive/github_repos/mjv_personal_code/installed_vim_plugins.txt
+    rm ~/Google\ Drive/github_repos/mjv_personal_code/installed_vim_plugins.txt
+end
+
 for i in */
+    echo "$i" >> ~/Google\ Drive/github_repos/mjv_personal_code/installed_vim_plugins.txt
     cd "$i"
     git pull
     cd -
 end
 
+echo "installed_vim_plugins.txt written to personal GitHub repo"
 cd $cwd
 set -e cwd
